b48228b1dddf61fc88f3144462c46f7a8109de33
Edwin Young <syoung@atlassian.com>
2023-05-09 16:29:28 +1000

    added db interface

---
+++ src/main/kotlin/com/example/repository/db/DatabaseAccessInterface.kt
@@ -0,0 +1,20 @@
+package com.example.repository.db
+
+import com.example.model.Account
+import com.example.model.Character
+import com.example.model.Session
+
+interface DatabaseAccessInterface {
+    fun createSession(acct: Account, char: Character): Session
+    fun saveNewSessionRetire(session: Session): Int
+    fun updateSession(session: Session)
+    fun searchSessions(acct: Account): List<Session>
+    fun removeSession(session: Session)
+    fun insertCharacter(acct: Account, char: Character): Int
+    fun deleteCharacter(char: Character) : Boolean
+    fun findCharactersByAccount(acct: Account): List<Character>
+    fun findCharacterByName(charName: String) : Character?
+    fun findAccountById(id: Int): Account
+    fun findAccountByLogin(login: String): Account?
+    fun insertAccount(login: String, password: String, isAdmin: Boolean): Account
+}
